1923602dd415b6ab9d365d7915daff416be4c9f9
bump-version <bump@version.com>
2020-10-02 14:15:18 +0000

    Version 0.0.26 [skip ci]

--- __snapshots__/all.ts.js
+++ __snapshots__/all.ts.js
@@ -17,9 +17,3 @@ exports['globFromGit glob relative paths with \'tests/**/*.txt\' 1'] = [
   "tests/dir/subDir/example/file.txt",
   "tests/dir/subDir/file.txt"
 ]
-
-exports['globFromGit glob relative paths with \'/Users/morse/Documents/GitHub/smartglob/tests/**/*.txt\' 1'] = [
-  "tests/dir/exampleFile.txt",
-  "tests/dir/subDir/example/file.txt",
-  "tests/dir/subDir/file.txt"
-]
